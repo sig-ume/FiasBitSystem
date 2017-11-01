bba39ee75ac2cdbd28d1ed248d9a8159ca02fb08
sig-ume <beyondthemoonface@gmail.com>
2017-11-01 23:48:42 +0900

    仮アイコン設定、実行/停止時にアイコン変更

--- SeleniumTest/src/main/java/jp/sigre/fbs/gui/FbsGui.java
+++ SeleniumTest/src/main/java/jp/sigre/fbs/gui/FbsGui.java
@@ -10,8 +10,10 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 import java.util.Timer;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -28,56 +30,62 @@ import jp.sigre.fbs.timer.FbsTimerTask;
 public class FbsGui extends JFrame implements ActionListener{
 
 	private boolean isActive = false;
-    JButton button = new JButton();
+	JButton button = new JButton();
 	Timer timer = new Timer();
 	static TradeController trade = new TradeController();
+	ClassLoader thisLoader = getClass().getClassLoader();
+	URL stopIconUrl = thisLoader.getResource("lib/stop.png");
+	URL startIconUrl = thisLoader.getResource("lib/running.png");
 
 	public FbsGui(String title){
-//		setTitle(title);
-//		setBounds(100, 100, 200, 160);
-//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//	    JPanel p1 = new JPanel();
-//	    p1.setBackground(Color.BLUE);
-//
-//	    JPanel p2 = new JPanel();
-//	    p2.setBackground(Color.ORANGE);
-//
-//	    Container contentPane = getContentPane();
-//	    contentPane.add(p1, BorderLayout.NORTH);
-//	    contentPane.add(p2, BorderLayout.SOUTH);
-	    setTitle(title);
-	    setBounds(100, 100, 500, 500);
-	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//		setTitle(title);
+		//		setBounds(100, 100, 200, 160);
+		//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//
+		//	    JPanel p1 = new JPanel();
+		//	    p1.setBackground(Color.BLUE);
+		//
+		//	    JPanel p2 = new JPanel();
+		//	    p2.setBackground(Color.ORANGE);
+		//
+		//	    Container contentPane = getContentPane();
+		//	    contentPane.add(p1, BorderLayout.NORTH);
+		//	    contentPane.add(p2, BorderLayout.SOUTH);
+		setTitle(title);
+		setBounds(100, 100, 500, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	    setLayout(new FlowLayout());
+		setLayout(new FlowLayout());
 
-	    JPanel p1 = new JPanel();
-	    p1.setPreferredSize(new Dimension(500, 50));
-	    //p1.setBackground(Color.BLUE);
-	    p1.setOpaque(false);
+		JPanel p1 = new JPanel();
+		p1.setPreferredSize(new Dimension(500, 50));
+		//p1.setBackground(Color.BLUE);
+		p1.setOpaque(false);
 
-	    JPanel p2 = new JPanel();
-	    p2.setPreferredSize(new Dimension(50, 100));
-	    p2.setBackground(Color.ORANGE);
+		JPanel p2 = new JPanel();
+		p2.setPreferredSize(new Dimension(50, 100));
+		p2.setBackground(Color.ORANGE);
 
-	    button.setText("START");
-	    button.setPreferredSize(new Dimension(460, 45));
+		button.setText("START");
+		button.setPreferredSize(new Dimension(460, 45));
 
-	    button.addActionListener(this);
-	    p1.add(button);
+		button.addActionListener(this);
+		p1.add(button);
 
-	    BevelBorder border = new BevelBorder(BevelBorder.RAISED);
-	    p2.setBorder(border);
+		BevelBorder border = new BevelBorder(BevelBorder.RAISED);
+		p2.setBorder(border);
 
-	    ConsolePanel console = new ConsolePanel(460, 400);
+		ConsolePanel console = new ConsolePanel(460, 400);
 
-	    Container contentPane = getContentPane();
-	    contentPane.add(p1, BorderLayout.NORTH);
-	    //contentPane.add(p1);
-	    //contentPane.add(p2);
-	    contentPane.add(console, BorderLayout.SOUTH);
+		Container contentPane = getContentPane();
+		contentPane.add(p1, BorderLayout.NORTH);
+		//contentPane.add(p1);
+		//contentPane.add(p2);
+		contentPane.add(console, BorderLayout.SOUTH);
 
+		//アプリアイコン設定
+		ImageIcon icon = new ImageIcon(stopIconUrl);
+		setIconImage(icon.getImage());
 	}
 	/**
 	 * @param args
@@ -90,17 +98,21 @@ public class FbsGui extends JFrame implements ActionListener{
 
 		if (!resultSetup) return;
 
-	    frame.setVisible(true);
+		frame.setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (false == isActive) {
 			button.setText("STOP");
+			ImageIcon icon = new ImageIcon(startIconUrl);
+			setIconImage(icon.getImage());
 			timer = new Timer();
 			timer.schedule(new FbsTimerTask(trade), 0, 20 * 60 * 1000);
 		} else {
 			button.setText("START");
+			ImageIcon icon = new ImageIcon(stopIconUrl);
+			setIconImage(icon.getImage());
 			System.out.println("stop....");
 			timer.cancel();
 		}
